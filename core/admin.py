5b07ee72f5647d338bdb654f954b210f0f6e469c
Nathan Ravenscraft <nraven22@gmail.com>
2021-01-07 11:17:59 -0500

    registered models to admin

--- core/admin.py
+++ core/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import User, Registry, Item
 
 # Register your models here.
-admin.register(Registry)
-admin.register(User)
-admin.register(Item)
+admin.site.register(Registry)
+admin.site.register(User)
+admin.site.register(Item)
