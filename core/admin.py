e47549ef0b31d730727d5bc7fa4194122255c188
Nathan Ravenscraft <nraven22@gmail.com>
2021-01-09 19:24:05 -0500

    added message model to admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Registry, Item
+from .models import User, Registry, Item, Message
 
 # Register your models here.
 admin.site.register(Registry)
 admin.site.register(User)
 admin.site.register(Item)
+admin.site.register(Message)
